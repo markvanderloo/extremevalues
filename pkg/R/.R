a8665c5b3357229d82107ae64236c538d20fc6d6
mark <mark.vanderloo@gmail.com>
2013-01-03 17:33:41 +0100

    improved outlier plots, documentation bugs

--- pkg/R/.R
+++
@@ -1,6 +0,0 @@
-
-
-source('plotpositions.R')
-
-
-
--- pkg/R/outliers.R
+++ pkg/R/outliers.R
@@ -10,7 +10,7 @@
 #' @param x numeric vector
 #' @param qfun quantile function of the distribution for which parameters must be estimated. It must accept a vector of
 #'    values between 0 and 1, and one or more named parameters as input. An example is \code{R}'s built-in \code{qnorm} function.
-#' @param Flim Determines which observations to use. By default all observations are used (between the 0th and 100th percentile).
+#' @param Flim Determines which observations to use. By default all observations are used (between the 10th and 90th percentile).
 #' @param ... initial values of the parameters
 #' @param optimpar A list of named parameters for R's \code{optim} function.
 #'
@@ -19,7 +19,7 @@
 #' @export
 #' @example ../examples/outliers.R
 #' 
-parameters <- function(x, qfun, Flim=c(0,1), ..., optimpar=list()){
+parameters <- function(x, qfun, Flim=c(0.1,0.9), ..., optimpar=list()){
    stopifnot(
       is.numeric(x),
       all_finite(x),
@@ -40,7 +40,7 @@ parameters <- function(x, qfun, Flim=c(0,1), ..., optimpar=list()){
    }
    L <- do.call(optim, c(list(par=unlist(list(...)), fn=minfun), optimpar))
 
-   L <- c(list(qfun=qfun, N=N, Flim=Flim, optimpar=optimpar), L)
+   L <- c(list(qfun=qfun, N=N, Flim=Flim, range=range(x), optimpar=optimpar), L)
    structure(L, class='parameters')
 }
 
@@ -49,10 +49,11 @@ parameters <- function(x, qfun, Flim=c(0,1), ..., optimpar=list()){
 #' @param object an object of class parameters, as returned by the \code{\link{parameters}} function.
 #' @param x vector of observations
 #' @param all Indicate wheter to return all residuals (including extrapolations) or just the residuals of observations used in the fit.
-#'@export
+#' @param ... parameters to be passed to or from other methods (none at this time)
+#' @export
 residuals.parameters <- function(object, x, all=TRUE, ...){
    Fhat <- plotpositions(x)
-   r <- do.call(object$qfun, c(list(Fhat), as.list(object$par)))
+   r <- x - do.call(object$qfun, c(list(Fhat), as.list(object$par)))
 
    if ( all){ 
       r
@@ -65,8 +66,8 @@ residuals.parameters <- function(object, x, all=TRUE, ...){
 
 #' Compute outlier limits based on fitted parameters
 #' 
-#' @param parameters An object of class 'parameters', as returned by \code{\link{parameters}}
-#' @param method Choose a method for determining the limit
+#' @param parameters An object of class \code{parameters}, as returned by \code{\link{parameters}}
+#' @param x The original vector with observations (only necessary when \code{method='residuals'})
 #' @param ... Arguments to be passed to methods for limit calculation. Specifically
 #'  \tabular{llll}{
 #'  Method  \tab parameter  \tab default \tab description \cr
@@ -74,31 +75,34 @@ residuals.parameters <- function(object, x, all=TRUE, ...){
 #'  residual \tab \code{alpha} \tab c(0.05,0.05) \tab interval on fit residuals
 #' }
 #' @export
-limits <- function(parameters, ...){
+limits <- function(parameters, x=NULL, ...){
    UseMethod('limits')
 }
 
 # poor-man's multiple dispatch.
-limits.parameters <- function(parameters, method=c('direct','residuals'), ...){
+limits.parameters <- function(parameters, x, method=c('direct','residuals'), ...){
    method <- match.arg(method)
+   if (method=='residuals' && is.null(x)){ 
+      stop('For residual method the vector of observations must be included')
+   }
    switch(method,
       direct      = direct_limits(parameters, ...),
-      residuals   = residual_limits(parameters, ...),
+      residuals   = residual_limits(parameters, x, ...),
       stop('unrecognized method')
    )  
 }
 
-direct_limits <- function(x, rho=c(0.5,0.5)){
-   p <- rho/x$N
+direct_limits <- function(v, rho=c(0.5,0.5)){
+   p <- rho/parameters$N
    
    c(
-      Lmin = do.call(x$qfun,c(list(p[1]),as.list(x$par))), 
-      Lplus= do.call(x$qfun,c(list(1-p[2]),as.list(x$par)))
+      Lmin = do.call(v$qfun,c(list(p[1]),as.list(v$par))), 
+      Lplus= do.call(v$qfun,c(list(1-p[2]),as.list(v$par)))
    )
 }
 
-residual_limits <- function(x, alpha=c(0.05,0.05)){
-   sigma <- sd(residuals(x,all=FALSE))
+residual_limits <- function(v, x, alpha=c(0.05,0.05)){
+   sigma <- sd(residuals(v, x, all=FALSE))
    sq2 <- sqrt(2)
    sq2*sigma*c(Lmin=invErf(2*alpha[1]-1), Lplus=invErf(1-2*alpha[2]) )
 }
@@ -108,6 +112,7 @@ residual_limits <- function(x, alpha=c(0.05,0.05)){
 #' @param parameters An object of class \code{parameters} as returned by 
 #'    the \code{\link{parameters}} function.
 #' @param x Vector of observations
+#' @param ... Arguments passed to \code{limits}
 #' @export
 is.outlier <- function(parameters, x, ...){
    L <- limits(parameters,...)
@@ -118,26 +123,28 @@ is.outlier <- function(parameters, x, ...){
 
 #' Plot data and indicate outliers.
 #'
-#' @param x An object of class \code{outliers} as returned by the
-#'  \code{\link{outliers}} function.
+#' @param x An object of class \code{parameters} as returned by the
+#'  \code{\link{parameters}} function.
 #' @param y A data vector
-#' @param ... 
+#' @param method The method for limit determination
+#' @param ...  Graphical arguments to be passed to 'plot'
 #' @export
 plot.parameters <- function(x, y, method=c('direct','residuals'), ...){
    method <- match.arg(method)
    switch(method,
-      direct = plot_direct(x,y,...),
+      direct      = plot_direct(x,y,...),
+      residuals   = plot_residuals(x,y,...),
       stop('Unknown method')
    )
 }
 
-plot_direct <- function(x,y,...){
+plot_direct <- function(x,y,rho=c(0.5,0.5),...){
    Fhat <- plotpositions(y)
-   yhat <- do.call(x$qfun, c(list(Fhat),as.list(x$par)))
-   L <- limits(x, y, method='direct')
+   yhat <- do.call(x$qfun, c(list(Fhat), as.list(x$par)))
+   L <- limits(x, method='direct',rho=rho)
    plotrange <- range(c(y,yhat))
    fitrange <- range(yhat[Fhat >= x$Flim[1] & Fhat <= x$Flim[2]])
-   plot(yhat,y,xlim=plotrange, ylim=plotrange)
+   plot(yhat,y,xlim=plotrange, ylim=plotrange,...)
    abline(a=0,b=1)
 
    color = 'gray'
@@ -156,14 +163,40 @@ plot_direct <- function(x,y,...){
       col=color
    )
    points(yhat,y,xlim=plotrange, ylim=plotrange)
-   abline(a=L[1],b=0,lty=2)
-   abline(a=L[2],b=0,lty=2)
+   abline(h=L[1],lty=2)
+   abline(h=L[2],lty=2)
    abline(v=fitrange[1],lty=2)
    abline(v=fitrange[2],lty=2)
-
 }
 
+plot_residuals <- function(x,y, alpha=c(0.05,0.05),...){
+   L <- limits(x, y, method='residuals',alpha=alpha)
+   
+   r <- residuals(x,y,all=TRUE)
+   plot(y,r,...)
 
+   color = 'gray'
+   # bottom coordinate
+   b <- par("usr")[c(1,3)]
+   polygon(
+      x = c(b[1], x$range[1], x$range[1], b[1]), 
+      y = c(b[2], b[2]    , L[1]      , L[1]  ),
+      col = color
+   )
+   # top coordinate
+   u <- par("usr")[c(2,4)]
+   polygon(
+      x   = c(x$range[2], u[1],u[1],x$range[2]),
+      y   = c(L[2]      , L[2],u[2],u[2]),
+      col = color
+   )
+   points(y,r,...)
+   abline(h=0)
+   abline(h=L[1],lty=2)
+   abline(h=L[2],lty=2)
+   abline(v=x$range[1],lty=2)
+   abline(v=x$range[2],lty=2)  
+}
 
 
 
